57b5b38a2a8c9c71e7d7c01224817d60981646e0
gabriel <gabrielalvesdossantos16@gmail.com>
2025-06-07 14:40:32 -0400

    Maps funcionando

--- App.tsx
+++ App.tsx
@@ -1,72 +1,66 @@
-import { useState } from 'react';
-import { View, StyleSheet, Button, PermissionsAndroid, Platform } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {styles}  from './styles/styles'
+import { requestForegroundPermissionsAsync, getCurrentPositionAsync, LocationObject, watchPositionAsync, LocationAccuracy } from 'expo-location';
+import { useEffect, useState, useRef } from 'react';
 import MapView, { Marker } from 'react-native-maps';
-import Geolocation from 'react-native-geolocation-service';
 
-const App = () => {
-  const [location, setLocation] = useState<{ latitude: number; longitude: number } | null>(null);
+export default function App() {
+const [location, setLocation] = useState<LocationObject | null>(null);
 
-  const pedirPermissao = async () => {
-    if (Platform.OS === 'android') {
-      const granted = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-      );
-      return granted === PermissionsAndroid.RESULTS.GRANTED;
+const mapRef = useRef<MapView>(null);
+
+  async function requestLocationPermition(){
+    const { status } = await requestForegroundPermissionsAsync();
+
+    if (status){
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
     }
-    return true;
-  };
+  }
 
-  const obterLocalizacao = async () => {
-    const temPermissao = await pedirPermissao();
-    if (!temPermissao) return;
+useEffect(()=>{
+  requestLocationPermition()
+},[])  
 
-    Geolocation.getCurrentPosition(
-      pos => {
-        const { latitude, longitude } = pos.coords;
-        setLocation({ latitude, longitude });
-      },
-      error => {
-        console.error(error);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  };
+useEffect(()=>{
+  watchPositionAsync({
+    accuracy: LocationAccuracy.Highest,
+    timeInterval: 1000,
+    distanceInterval: 1
+  },(response)=>{
+   console.log(response)
+   setLocation(response)
+   mapRef.current?.animateCamera({
+    pitch:90,
+    center: response.coords
+   })
+  })
+},[])  
+
+
 
   return (
     <View style={styles.container}>
-      <MapView
-        style={styles.mapa}
-        region={
-          location
-            ? {
-                latitude: location.latitude,
-                longitude: location.longitude,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01,
-              }
-            : {
-                latitude: -23.55052,
-                longitude: -46.633308,
-                latitudeDelta: 0.1,
-                longitudeDelta: 0.1,
-              }
-        }
-        showsUserLocation
-      >
-        {location && <Marker coordinate={location} />}
-      </MapView>
-      <Button title="Obter Localização Atual" onPress={obterLocalizacao} />
+     { location &&
+
+<MapView
+ref={mapRef}
+style={styles.map}
+initialRegion={{
+latitude: location. coords. latitude,
+longitude: location. coords.longitude,
+latitudeDelta: 0.0005,
+longitudeDelta: 0.0005
+}}> 
+<Marker
+coordinate={{
+latitude: location. coords. latitude,
+longitude: location. coords.longitude,}}>
+  </Marker>
+  </MapView>
+}
     </View>
   );
-};
+}
 
-export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  mapa: {
-    flex: 1,
-  },
-});
