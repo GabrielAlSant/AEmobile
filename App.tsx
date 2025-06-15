a95ddd73543519167647179c6ce5b8408a73c039
gabriel <gabrielalvesdossantos16@gmail.com>
2025-06-15 09:42:17 -0400

    Componentes criados e Router

--- App.tsx
+++ App.tsx
@@ -1,76 +1,33 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View } from "react-native";
-import { styles } from "./styles/styles";
-import {
-  requestForegroundPermissionsAsync,
-  getCurrentPositionAsync,
-  LocationObject,
-  watchPositionAsync,
-  LocationAccuracy,
-} from "expo-location";
-import { useEffect, useState, useRef } from "react";
-import MapView from "react-native-maps";
-import Maps from './components/map';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import CurrentLocationPage from "./src/CurrentLocationPage";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
-  const [location, setLocation] = useState<LocationObject | null>(null);
-  const [markers, setMarkers] = useState<
-    { latitude: number; longitude: number; criticidade: number }[]
-  >([]);
+  const Tab = createBottomTabNavigator();
 
-  const mapRef = useRef<MapView>(null);
-
-  async function requestLocationPermition() {
-    const { status } = await requestForegroundPermissionsAsync();
-
-    if (status) {
-      const currentPosition = await getCurrentPositionAsync();
-      setLocation(currentPosition);
-    }
-  }
-
-  useEffect(() => {
-    requestLocationPermition();
-  }, []);
-
-  useEffect(() => {
-    watchPositionAsync(
-      {
-        accuracy: LocationAccuracy.Highest,
-        timeInterval: 1000,
-        distanceInterval: 1,
-      },
-      (response) => {
-        console.log(response);
-        setLocation(response);
-        mapRef.current?.animateCamera({
-          pitch: 90,
-          center: response.coords,
-        });
-      }
+  function MyTabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen name="Conversor" component={CurrentLocationPage} />
+      </Tab.Navigator>
     );
-  }, []);
-
-  async function handleLongPress(event: any) {
-    const currentPosition = await getCurrentPositionAsync();
-    setMarkers((prev) => [
-      ...prev,
-      {
-        latitude: currentPosition.coords.latitude,
-        longitude: currentPosition.coords.longitude,
-        criticidade: 2,
-      },
-    ]);
   }
-
-  useEffect(() => {
-    console.log(markers);
-  }, [markers]);
 
   return (
-    <View style={styles.container}>
-     <Maps markers={markers} location={location} mapRef={mapRef} />
-      <Button title="Marcar Localização" onPress={handleLongPress} />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8,
+  },
+});
---
+++ src/CurrentLocationPage.tsx
@@ -0,0 +1,76 @@
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { styles } from "../styles/styles";
+import {
+  requestForegroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+} from "expo-location";
+import { useEffect, useState, useRef } from "react";
+import MapView from "react-native-maps";
+import Maps from '../components/map';
+
+export default function CurrentLocationPage() {
+  const [location, setLocation] = useState<LocationObject | null>(null);
+  const [markers, setMarkers] = useState<
+    { latitude: number; longitude: number; criticidade: number }[]
+  >([]);
+
+  const mapRef = useRef<MapView>(null);
+
+  async function requestLocationPermition() {
+    const { status } = await requestForegroundPermissionsAsync();
+
+    if (status) {
+      const currentPosition = await getCurrentPositionAsync();
+      setLocation(currentPosition);
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermition();
+  }, []);
+
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (response) => {
+        console.log(response);
+        setLocation(response);
+        mapRef.current?.animateCamera({
+          pitch: 90,
+          center: response.coords,
+        });
+      }
+    );
+  }, []);
+
+  async function handleLongPress(event: any) {
+    const currentPosition = await getCurrentPositionAsync();
+    setMarkers((prev) => [
+      ...prev,
+      {
+        latitude: currentPosition.coords.latitude,
+        longitude: currentPosition.coords.longitude,
+        criticidade: 2,
+      },
+    ]);
+  }
+
+  useEffect(() => {
+    console.log(markers);
+  }, [markers]);
+
+  return (
+    <View style={styles.container}>
+     <Maps markers={markers} location={location} mapRef={mapRef} />
+      <Button title="Marcar Localização" onPress={handleLongPress} />
+    </View>
+  );
+}
